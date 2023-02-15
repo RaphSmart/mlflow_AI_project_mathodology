28749b8b17d0accdedc8e0b170821040c2414ab9
Sista88 <105207251+Sista88@users.noreply.github.com>
2023-02-15 15:06:19 +0100

    uploading model evaluation script

---
+++ Different scripts/metrics_evaluation.py
@@ -0,0 +1,5 @@
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
